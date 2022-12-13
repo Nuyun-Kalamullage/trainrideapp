27b90a940aa0d685c511f8bade090ad0fd1e3826
Nuyun Kalamullage <73055937+Nuyun-Kalamullage@users.noreply.github.com>
2022-12-13 12:33:53 +0530

    implementing google maps to homepage.

---
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -0,0 +1,44 @@
+//
+// Generated file. Do not edit.
+// This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
+//
+
+// @dart = 2.18
+
+import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart';
+
+@pragma('vm:entry-point')
+class _PluginRegistrant {
+
+  @pragma('vm:entry-point')
+  static void register() {
+    if (Platform.isAndroid) {
+      try {
+        GoogleMapsFlutterAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isIOS) {
+      try {
+        GoogleMapsFlutterIOS.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isLinux) {
+    } else if (Platform.isMacOS) {
+    } else if (Platform.isWindows) {
+    }
+  }
+}
